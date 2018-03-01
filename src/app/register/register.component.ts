0633ada9fcae9584a0a5be88ea69b4014305edd9
Paras Sharma <paras16jul1991@gmail.com>
2018-03-01 16:31:54 +0530

    Update register.component.ts

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    model: any = {};
+    loading = false;
+    error = '';
+ 
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService) { }
+ 
+    ngOnInit() {
+        // reset login status
+       // this.authenticationService.logout();
+    }
+ 
+    signup() {
+        this.loading = true;
+        this.authenticationService.signup(this.model.username, this.model.password)
+            .subscribe(result => {
+                if (result === true) {
+                    // login successful
+                    this.router.navigate(['/home']);
+                } else {
+                    // login failed
+                    this.error = 'Username or password is incorrect';
+                    this.loading = false;
+                }
+            });
+    }
 }
