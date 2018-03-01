bf2a856b52ee85fdc3b213efca5da26bc49a222c
Paras Sharma <paras16jul1991@gmail.com>
2018-03-01 16:43:57 +0530

    Update UserController.java

--- src/main/java/com/splitmoney/UserController.java
+++ src/main/java/com/splitmoney/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
 public class UserController {
 	private ApplicationUserRepository applicationUserRepository;
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
